c01f6c529e55c5a226fa193118704bd1539f4bdf
NearKong <Near.Kong@Near-Pro.local>
2016-11-28 23:12:02 +0800

    提交

--- NearCameraManager/AppDelegate.m
+++ NearCameraManager/AppDelegate.m
@@ -20,6 +20,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
 
+    
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.backgroundColor = [UIColor whiteColor];
 
